d804e02990a4aaae95b98f6bbb007ac44ff4f17d
Z14010040 <xiao120315>
2021-02-04 22:20:08 +0800

    测试方法更新

---
+++ src/test/java/com/brilliance/netsign/NetsignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brilliance.netsign;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetsignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
