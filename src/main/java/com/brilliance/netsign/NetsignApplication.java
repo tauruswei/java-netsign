51e3b601b1e3a85982ecec5fd6cb079f7a4655c0
Z14010040 <xiao120315>
2021-08-06 14:46:51 +0800

    http 服务 改成 https 服务

--- src/main/java/com/brilliance/netsign/NetsignApplication.java
+++ src/main/java/com/brilliance/netsign/NetsignApplication.java
@@ -1,8 +1,15 @@
 package com.brilliance.netsign;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @MapperScan("com.brilliance.netsign.dao")
@@ -10,6 +17,40 @@ public class NetsignApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NetsignApplication.class, args);
+    }
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+
+    }
+
+    /**
+     * 让我们的应用支持HTTP是个好想法，但是需要重定向到HTTPS，
+     * 但是不能同时在application.properties中同时配置两个connector，
+     * 所以要以编程的方式配置HTTP connector，然后重定向到HTTPS connector
+     * @return Connector
+     */
+    private Connector redirectConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(19445); // http端口
+        connector.setSecure(false);
+        connector.setRedirectPort(19444); // application.properties中配置的https端口
+        return connector;
     }
 
 }
